8aad0825d10498ad1614824c7e6c23be542316b6
dzm11 <piotr.brze95@gmail.com>
2021-10-18 23:03:30 +0200

    clamp

--- app/js/script.js
+++ app/js/script.js
@@ -21,22 +21,19 @@ burger.addEventListener("click", () => {
 })
 var swiper = new Swiper(".mySwiper", {
     slidesPerView: 1,
+    spaceBetween: 0,
     pagination: {
-      el: ".swiper-pagination",
       clickable: true,
     },
     breakpoints: {
       640: {
         slidesPerView: 2,
-        spaceBetween: 20,
       },
       768: {
         slidesPerView: 4,
-        spaceBetween: 40,
       },
       1024: {
         slidesPerView: 5,
-        spaceBetween: 50,
       },
     },
   });
