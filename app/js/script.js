ad6be613bf01714a8a748412f0e19aeb6192f233
dzm11 <piotr.brze95@gmail.com>
2021-10-21 20:17:20 +0200

    mobilka dziala

--- app/js/script.js
+++ app/js/script.js
@@ -22,8 +22,9 @@ burger.addEventListener("click", () => {
 var swiper = new Swiper(".mySwiper", {
     slidesPerView: 1,
     spaceBetween: 0,
-    pagination: {
-      clickable: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
     },
     breakpoints: {
       600:{
